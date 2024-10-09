6479ea14354e77a6c9cf984d45c962c03593127d
maddyholden <madeleineholdenn@gmail.com>
2024-10-09 19:27:46 -0400

    Updated detector.py

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -4,11 +4,33 @@ from teams_classes import DetectionMark
 class Detector(ADetector):
     def detect_bot(self, session_data):
         # todo logic
-        # Example:
-        marked_account = [
-            DetectionMark(user_id='8d925460-8b23-4f12-a07e-2541891c7ef1', confidence=90, bot=True),
-            DetectionMark(user_id='6e28897d-77db-4292-a3b4-c4f35fe55f7f', confidence=100, bot=True)
-        ]
+        marked_account = [] # Empty list to put DetectionMark objects
+
+        # CHECKING FOR BOT IN USERS:
+        users=session_data.users
+        for user in users:
+            is_bot=False
+            #print("User #", users.index(user)+1)
+            for item in user:
+                #print("\t", item, ":", user[item])
+                if "bot" in str(user[item]).lower(): # Check if item contains the word "bot"
+                    is_bot=True # If it does, set is_bot to True
+                # Check if ID contains any letters or special characters
+                if item=="id" and not user["id"].isalnum():
+                    is_bot=True
+            if is_bot:
+                marked_account.append(DetectionMark(user_id=user["id"], confidence=75, bot=True))
+            else:
+                marked_account.append(DetectionMark(user_id=user["id"], confidence=75, bot=False))
+
+        # CHECKING FOR BOT IN POSTS:
+        posts=session_data.posts
+        for post in posts:
+            for item in post:
+                if "bot" in str(post[item]).lower(): # Check if item contains the word "bot"
+                    # CHECK IF USER IS ALREADY IN MARKED ACCOUNT
+                    if DetectionMark(user_id=post["author_id"], confidence=75, bot=True) not in marked_account:
+                        marked_account.append(DetectionMark(user_id=post["author_id"], confidence=75, bot=True))
 
         return marked_account
     
